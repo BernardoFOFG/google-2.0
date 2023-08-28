73c6ef2b22fe4281b2a09afe59ca24df7e51dc5a
BernardoFOFG <bernardofofg@gmail.com>
2023-08-28 10:36:18 -0300

    feat: finish google front-end

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { Avatar } from "@/components/Avatar";
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
+import { SearchIcon } from '@heroicons/react/outline'
+import Image from "next/image";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="bg-white h-screen">
-      {/* Header   */}
-
-      <header className="flex justify-between p-5 text-sm text-gray-700">
+    <main className="flex flex-col items-center justify-center h-screen">
+      <header className="flex w-full justify-between p-5 text-sm text-gray-700">
         <div className="flex space-x-4 items-center">
           <p className="link">About</p>
           <p className="link">Store</p>
@@ -13,17 +15,26 @@ export default function Home() {
         <div className="flex space-x-4 items-center">
           <div className="link">Gmail</div>
           <div className="link">Images</div>
-
-          {/* Icon */}
-
-          {/* Avatar */}
-
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
           <Avatar url='https://github.com/bernardofofg.png' />
         </div>
       </header>
-      {/* Body */}
+      <form className="flex flex-col items-center mt-32 flex-grow w-4/5">
+        <Image src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" width={300} height={100} />
 
-      {/* Footer */}
+        <div className="flex justify-center items-center w-full max-w-md mt-5 px-5 py-3 border border-gray-200 rounded-full hover:shadow-lg focus-within:shadow-lg sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="focus:outline-none flex-grow" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sn:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm Felling Lucky</button>
+        </div>
+      </form>
+
+      <Footer />
     </main>
   );
 }
