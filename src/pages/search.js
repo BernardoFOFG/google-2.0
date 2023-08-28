4f11ea9aebb6ab8f378c18324296286ae6e3d317
BernardoFOFG <bernardofofg@gmail.com>
2023-08-28 11:28:12 -0300

    add: search component

---
+++ src/pages/search.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export const Search = () => {
+  return (
+    <div>
+      <Head>
+        <title>Search Results</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+
+      {/* Search Results */}
+    </div>
+  )
+}
