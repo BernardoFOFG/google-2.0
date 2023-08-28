cfe09fb12aa152997f45870d6ae2f3fd62b4151e
BernardoFOFG <bernardofofg@gmail.com>
2023-08-28 17:46:09 -0300

    api keys

---
+++ keys.js
@@ -0,0 +1,4 @@
+// API Key -> https://www.googleapis.com/customsearch/v1?key=AIzaSyBy5Wt0pHkMn7ZGY5vtugAoWfIIgNsaXms&cx=017576662512468239146:omuauf_lfve&q=lectures
+export const API_KEY = "AIzaSyBy5Wt0pHkMn7ZGY5vtugAoWfIIgNsaXms"
+// Context Key -> https://cse.google.com/cs/create/new
+export const CONTEXT_KEY = "2520e439e200f4a9f"
